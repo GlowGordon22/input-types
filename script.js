2b88850991984585307f6e535c9c406be1ef7fe6
GlowGordon22 <120579881+GlowGordon22@users.noreply.github.com>
2025-01-06 09:14:11 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-const submitButton
+const submitButton = document.getElementById('btn');
+
+submitButton.addEventListener('click', () => {
+  console.log('Form has been submitted')
+  const Pin = prompt("Please enter the Pin!")
+  
+  const validPins = ["admin", "administrator", "McDonalds Drive Thru"]
+  if (validPins.includes(Pin)) {
+    alert('Correct Pin!')
+    window.location.href = "roblox.com"
+  } else {
+    alert('Incorrect Pin')
+  }
+});
